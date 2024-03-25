2f50ecf39ab71f743a5396ea90d678fc58a38308
Oleksandr_Povidzion <ICOleksandr.Povidzion@Americanwell.com>
2024-03-25 14:17:24 +0200

    Initial setup

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 20000,
+  requestTimeout: 20000,
+  //reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'cypress/reporter-config.json',
+  },
+  retries: {
+    runMode: 1,
+    openMode: 0,
+  },
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  video: false,
+  scrollBehavior: 'center',
+  e2e: {
+    baseUrl: 'https://reportportal.epam.com/ui',
+  },
+});
